5987f8eb92cefcf38b066c8bf44571301c7ff759
ShahainManujith <86903975+ShahainManujith@users.noreply.github.com>
2021-11-02 22:06:08 +0530

    Test Comment Updated

--- Marha.js
+++ Marha.js
@@ -5,6 +5,7 @@ Target.addEventListener(
 
     (e) => {
         if (e.key === "Enter") {
+            // console.log("Reached");
             location.href = "https://www.marhaonline.com/";
         }
         e.stopPropagation();
